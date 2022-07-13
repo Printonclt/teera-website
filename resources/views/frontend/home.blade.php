4925e7ff3a98d586874e1d6f45d3ec51a07c1e23
Muhammed Rashid <rashidpvr446@gmail.com>
2022-07-13 21:13:39 +0530

    home page banner created

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -1,6 +1,64 @@
 @extends('frontend.layouts.app')
+@section('css')
+<link
+  rel="stylesheet"
+  href="https://unpkg.com/swiper@8/swiper-bundle.min.css"
+/>
+
+
+@endsection
 @section('content')
-   <div class="container">
-    <P class="m-5 p-3 text-center">Teera Home Page</P>
-    </div> 
+<div class="hero">
+    <div class="swiper">
+        <!-- Additional required wrapper -->
+        <div class="swiper-wrapper">
+          <!-- Slides -->
+          <div class="swiper-slide">
+            <div class="hero-content">
+                <h2 class="hero-heading white-color">Lorem Ipsum Dolersit Amet</h2>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's</p>
+                <div class="btn-wrapper">
+                    <a href="Read More" class="primary-button">Read More</a>
+                </div>
+            </div>
+            <img src="{{asset('images/banner/banner.jpg')}}" alt="" srcset="">
+          </div>
+          <div class="swiper-slide">
+            <img src="{{asset('images/banner/banner.jpg')}}" alt="" srcset="">
+          </div>
+          <div class="swiper-slide">
+            <img src="{{asset('images/banner/banner.jpg')}}" alt="" srcset="">
+          </div>
+         
+        
+        </div>
+        <!-- If we need pagination -->
+        <div class="swiper-pagination"></div>
+      
+        
+      </div>
+</div>
+ <!-- Slider main container -->
+
+@endsection
+
+@section('script')
+
+<script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
+<script>
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+   
+   
+  });
+</script>
+
 @endsection
--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -5,9 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{asset('css/common/bootstrap/bootstrap.min.css')}}">
+   
+  
+    <title>@yield('title')</title>
     @yield('css')
     <link rel="stylesheet" href="{{asset('css/style.css')}}">
-    <title>@yield('title')</title>
 </head>
 <body>
  
