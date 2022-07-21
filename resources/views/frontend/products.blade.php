b39092f017e1c2a73a19c4532beaae4c2a2b1b05
Anuraj <anurajkv1999@gmail.com>
2022-07-21 11:10:03 +0530

    Product page changes

--- resources/views/frontend/products.blade.php
+++ resources/views/frontend/products.blade.php
@@ -11,74 +11,97 @@
 
         <div class="container">
             <div class="product-content">
-                <h4>Plates</h4>
+                <h4>Rice Straw</h4>
             </div>
 
             <div class="row align-items-center mt-5">
                 <div class="col-md-3">
                     <div class="plate-img">
-                        <img src="{{ asset('/images/banner/plate.png') }}" alt="">
+                        <img src="{{ asset('/images/products/Rice Straw 6.5mm x 200mm.jpg') }}" alt="">
 
                         <div class="plate-content">
-                            <h6>Plate</h6>
-                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam, dolore architecto, asperiores
-                                dolorem doloremque rerum, consectetur voluptatibus vel fugit harum magnam unde vero quasi
-                                quaerat aspernatur! Voluptas quod molestias qui.</p>
+                            <h6>Rice Straw 6.5mm x 200mm</h6>
+                            <span>Now @ ₹2/straw</span>
+                            <span>Suitable For - Soft Drinks & Non Pulpy Juices</span>
+                            <p>Smaart Rice Straws are a perfect alternative to plastic, paper, bamboo and metal straws. Our
+                                straws are eco-friendly, sustainable, biodegradable. Let's pledge to reduce plastic waste
+                                and join hands together to save our mother Earth.</p>
+                            <span>INGREDIENTS :</span>
+                            <p>Rice Flour, Tapioca Starch, Natural Colors, E 45 l i, E 202.</p>
+                            <span>USAGE INSTRUCTIONS : </span>
+                            <p>Use straw for drinks with temperature between 5 to 30 degree C for duration of 45 – 60 mins.
+                                Straw can cracked or bloat if used for more than 60 mins. Straws cannot be reused.</p>
+                            <p> 1 BOX CONTAINS: 2400 PCS <br>
+                                (12 PACKS X 200 PCS EACH)</p>
+                            <span>₹4,800.00</span>
+
                         </div>
                     </div>
                 </div>
                 <div class="col-md-3">
                     <div class="plate-img">
-                        <img src="{{ asset('/images/banner/plate.png') }}" alt="">
+                        <img src="{{ asset('/images/products/Rice-Straw-8mm-x-200mm.jpg') }}" alt="">
                         <div class="plate-content">
-                            <h6>Plate</h6>
-                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam, dolore architecto, asperiores
-                                dolorem doloremque rerum, consectetur voluptatibus vel fugit harum magnam unde vero quasi
-                                quaerat aspernatur! Voluptas quod molestias qui.</p>
+                            <h6>Rice Straw 8mm x 200mm</h6>
+                            <span>Now @ ₹2/straw</span>
+                            <span>Suitable For – Soft Drinks, Juices, Cold Coffee, Cocktails and Mocktails.</span>
+                            <p>Smaart Rice Straws are a perfect alternative to plastic, paper, bamboo and metal straws. Our
+                                straws are eco-friendly, sustainable, biodegradable. Let’s pledge to reduce plastic waste
+                                and join hands together to save our mother Earth</p>
+                            <span>INGREDIENTS :</span>
+                            <p>Rice Flour, Tapioca Starch, Natural Colors, E 45 l i, E 202.</p>
+                            <span>USAGE INSTRUCTIONS : </span>
+                            <p>Use straw for drinks with temperature between 5 to 30 degree C for duration of 45 – 60 mins.
+                                Straw can cracked or bloat if used for more than 60 mins. Straws cannot be reused.</p>
+                            <p> 1 BOX CONTAINS: 2000 PCS<br>
+                                (15 PACKS X 135 PCS EACH)</p>
+                            <span>₹4,000.00</span>
+
+                        </div>
+
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="plate-img">
+                        <img src="{{ asset('/images/products/Rice-Straw-10mm-x-200mm.jpg') }}" alt="">
+                        <div class="plate-content">
+                            <h6>Rice Straw 10mm x 200mm</h6>
+                            <span>Now @ ₹2.30/straw</span>
+                            <span>Suitable For - Milk Shakes and Thick Shakes.</span>
+                            <p>Smaart Rice Straws are a perfect alternative to plastic, paper, bamboo and metal straws. Our
+                                straws are eco-friendly, sustainable, biodegradable. Let’s pledge to reduce plastic waste
+                                and join hands together to save our mother Earth.</p>
+                            <span>INGREDIENTS :</span>
+                            <p>Rice Flour, Tapioca Starch, Natural Colors, E 45 l i, E 202.</p>
+                            <span>USAGE INSTRUCTIONS : </span>
+                            <p>Use straw for drinks with temperature between 5 to 30 degree C for duration of 45 – 60 mins.
+                                Straw can cracked or bloat if used for more than 60 mins. Straws cannot be reused</p>
+                            <p> 1 BOX CONTAINS: 1500 PCS<br>
+                                (15 PACKS X 100 PCS EACH)</p>
+                            <span>₹3,500.00</span>
+
                         </div>
                     </div>
                 </div>
                 <div class="col-md-3">
                     <div class="plate-img">
-                        <img src="{{ asset('/images/banner/plate.png') }}" alt="">
+                        <img src="{{ asset('/images/products/Rice-Straw-13mm-x-200mm.jpg') }}" alt="">
                         <div class="plate-content">
-                            <h6>Plate</h6>
-                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam, dolore architecto, asperiores
-                                dolorem doloremque rerum, consectetur voluptatibus vel fugit harum magnam unde vero quasi
-                                quaerat aspernatur! Voluptas quod molestias qui.</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-3">
-                    <div class="plate-img">
-                        <img src="{{ asset('/images/banner/plate.png') }}" alt="">
-                        <div class="plate-content">
-                            <h6>Plate</h6>
-                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam, dolore architecto, asperiores
-                                dolorem doloremque rerum, consectetur voluptatibus vel fugit harum magnam unde vero quasi
-                                quaerat aspernatur! Voluptas quod molestias qui.</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-3">
-                    <div class="plate-img">
-                        <img src="{{ asset('/images/banner/plate.png') }}" alt="">
-                        <div class="plate-content">
-                            <h6>Plate</h6>
-                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam, dolore architecto, asperiores
-                                dolorem doloremque rerum, consectetur voluptatibus vel fugit harum magnam unde vero quasi
-                                quaerat aspernatur! Voluptas quod molestias qui.</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-3">
-                    <div class="plate-img">
-                        <img src="{{ asset('/images/banner/plate.png') }}" alt="">
-                        <div class="plate-content">
-                            <h6>Plate</h6>
-                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam, dolore architecto, asperiores
-                                dolorem doloremque rerum, consectetur voluptatibus vel fugit harum magnam unde vero quasi
-                                quaerat aspernatur! Voluptas quod molestias qui.</p>
+                            <h6>Rice Straw 13mm x 200mm</h6>
+                            <span>Now @ ₹3/straw</span>
+                            <span>Suitable For – Bubble Teas and Falooda.</span>
+                            <p>Smaart Rice Straws are a perfect alternative to plastic, paper, bamboo and metal straws. Our
+                                straws are eco-friendly, sustainable, biodegradable. Let’s pledge to reduce plastic waste
+                                and join hands together to save our mother Earth.</p>
+                            <span>INGREDIENTS :</span>
+                            <p>Rice Flour, Tapioca Starch, Natural Colors, E 45 l i, E 202.</p>
+                            <span>USAGE INSTRUCTIONS : </span>
+                            <p>Use straw for drinks with temperature between 5 to 30 degree C for duration of 45 – 60 mins.
+                                Straw can cracked or bloat if used for more than 60 mins. Straws cannot be reused.</p>
+                            <p> 1 BOX CONTAINS: 1000 PCS<br>
+                                (18 PACKS X 56 PCS EACH)</p>
+                            <span>₹3,000.00</span>
+
                         </div>
                     </div>
                 </div>
